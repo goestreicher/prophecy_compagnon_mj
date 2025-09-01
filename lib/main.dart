e02f5ab0e103f82740d4a6cfd1a52dd233764c0e
Grégory Oestreicher <8679169+goestreicher@users.noreply.github.com>
2025-09-01 22:16:02 +0200

    Async load of assets at startup

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ import 'ui/scenario/list.dart';
 import 'ui/session/list.dart';
 import 'ui/spells/main.dart';
 import 'ui/table/list.dart';
+import 'ui/utils/full_page_loading.dart';
 
 void main() async {
   await DataStorage.instance.init();
@@ -55,6 +56,22 @@ class ProphecyCompanionApp extends StatelessWidget {
   }
 }
 
+Future<void> _loadAssets() async {
+  // TODO: some of those can throw storage exceptions, manage them
+  await Place.loadDefaultAssets();
+  await Place.loadStoreAssets();
+  await ArmorModel.loadDefaultAssets();
+  await CreatureModel.loadDefaultAssets();
+  await CreatureModel.loadStoreAssets();
+  await MagicSpell.loadDefaultAssets();
+  await NonPlayerCharacter.loadDefaultAssets();
+  await NonPlayerCharacter.loadStoreAssets();
+  await Faction.loadDefaultAssets();
+  await Faction.loadStoreAssets();
+  await ShieldModel.loadDefaultAssets();
+  await WeaponModel.loadDefaultAssets();
+}
+
 class MainAppPage extends StatefulWidget {
   const MainAppPage({super.key});
 
@@ -67,119 +84,110 @@ class _MainAppPageState extends State<MainAppPage> {
 
   @override
   Widget build(BuildContext context) {
-    /*
-      Load the default assets
-     */
-    // TODO: some of those can throw storage exceptions, manage them
-    // TODO: all of those are async, use a FutureBuilder to build the UI
-    Place.loadDefaultAssets();
-    Place.loadStoreAssets();
-    ArmorModel.loadDefaultAssets();
-    CreatureModel.loadDefaultAssets();
-    CreatureModel.loadStoreAssets();
-    MagicSpell.loadDefaultAssets();
-    NonPlayerCharacter.loadDefaultAssets();
-    NonPlayerCharacter.loadStoreAssets();
-    Faction.loadDefaultAssets();
-    Faction.loadStoreAssets();
-    ShieldModel.loadDefaultAssets();
-    WeaponModel.loadDefaultAssets();
-
-    var theme = Theme.of(context);
-    Widget activePage;
-
-    switch(_selectedPageIndex) {
-      case 0:
-        activePage = const SessionsListPage();
-        break;
-      case 1:
-        activePage = const TablesListPage();
-        break;
-      case 2:
-        activePage = const ScenariosListPage();
-        break;
-      case 3:
-        activePage = const NPCMainPage();
-        break;
-      case 4:
-        activePage = const CreaturesMainPage();
-        break;
-      case 5:
-        activePage = const PlacesMainPage();
-        break;
-      case 6:
-        activePage = FactionsMainPage();
-        break;
-      case 7:
-        activePage = const SpellsMainPage();
-        break;
-      default:
-        throw UnimplementedError('Page not implemented for $_selectedPageIndex');
-    }
-
-    return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 800,
-                  selectedIndex: _selectedPageIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      _selectedPageIndex = value;
-                    });
-                  },
-                  destinations: const [
-                    NavigationRailDestination(
-                      icon: Icon(Symbols.tactic_rounded),
-                      label: Text('Sessions'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Symbols.groups),
-                      label: Text('Tables'),
-                    ),
-                    NavigationRailDestination(
-                        icon: Icon(Symbols.book),
-                        label: Text('Scénarios'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Symbols.face),
-                      label: Text('PNJs'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(CustomIcons.creature),
-                      label: Text('Créatures'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.place_outlined),
-                      label: Text('Lieux'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Symbols.graph_2),
-                      label: Text('Factions'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Symbols.auto_fix_high),
-                      label: Text('Sorts'),
-                    ),
-                  ],
-                )
-              ),
-              Expanded(
-                child: ColoredBox(
-                  color: theme.colorScheme.surfaceContainerHighest,
-                  child: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 200),
-                    child: activePage,
-                  )
-                )
-              ),
-            ],
-          );
+    return FutureBuilder(
+      future: _loadAssets(),
+      builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+        if(snapshot.connectionState != ConnectionState.done) {
+          return FullPageLoadingWidget();
         }
-      )
+
+        var theme = Theme.of(context);
+        Widget activePage;
+
+        switch(_selectedPageIndex) {
+          case 0:
+            activePage = const SessionsListPage();
+            break;
+          case 1:
+            activePage = const TablesListPage();
+            break;
+          case 2:
+            activePage = const ScenariosListPage();
+            break;
+          case 3:
+            activePage = const NPCMainPage();
+            break;
+          case 4:
+            activePage = const CreaturesMainPage();
+            break;
+          case 5:
+            activePage = const PlacesMainPage();
+            break;
+          case 6:
+            activePage = FactionsMainPage();
+            break;
+          case 7:
+            activePage = const SpellsMainPage();
+            break;
+          default:
+            throw UnimplementedError('Page not implemented for $_selectedPageIndex');
+        }
+
+        return Scaffold(
+          body: LayoutBuilder(
+            builder: (context, constraints) {
+              return Row(
+                children: [
+                  SafeArea(
+                    child: NavigationRail(
+                      extended: constraints.maxWidth >= 800,
+                      selectedIndex: _selectedPageIndex,
+                      onDestinationSelected: (value) {
+                        setState(() {
+                          _selectedPageIndex = value;
+                        });
+                      },
+                      destinations: const [
+                        NavigationRailDestination(
+                          icon: Icon(Symbols.tactic_rounded),
+                          label: Text('Sessions'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Symbols.groups),
+                          label: Text('Tables'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Symbols.book),
+                          label: Text('Scénarios'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Symbols.face),
+                          label: Text('PNJs'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(CustomIcons.creature),
+                          label: Text('Créatures'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Icons.place_outlined),
+                          label: Text('Lieux'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Symbols.graph_2),
+                          label: Text('Factions'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Symbols.auto_fix_high),
+                          label: Text('Sorts'),
+                        ),
+                      ],
+                    )
+                  ),
+                  Expanded(
+                    child: ColoredBox(
+                      color: theme.colorScheme.surfaceContainerHighest,
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 200),
+                        child: activePage,
+                      )
+                    )
+                  ),
+                ],
+              );
+            }
+          )
+        );
+      }
     );
   }
 }
